af3ef2a6e134e16ce857b20ad3283361d392fb29
Ram Luis <ramluismarmol.edu@gmail.com>
2025-10-04 12:33:39 +0800

    Fixed warnings, errors and others

--- mysql/4_load_data.sql
+++ mysql/4_load_data.sql
@@ -1,62 +1,73 @@
 USE northwind_traders;
 -- Step 4, load the data from the CSV files in /data into the database
 
+SET FOREIGN_KEY_CHECKS = 0; -- Sets this off first so that we can insert to the table
+
 -- Load data into categories
-LOAD DATA LOCAL INFILE '../data/categories.csv'
+LOAD DATA LOCAL INFILE '<insert absolute path here>'
 INTO TABLE categories
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
 (categoryID, categoryName, description);
 
 -- Load data into customers
-LOAD DATA LOCAL INFILE '../data/customers.csv'
+LOAD DATA LOCAL INFILE '<insert absolute path here>'
 INTO TABLE customers
+CHARACTER SET latin1 -- Customer names contains special characters that MySQL cannot import, this normalizes it
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
 (customerID, companyName, contactName, contactTitle, city, country);
 
 -- Load data into employees
-LOAD DATA LOCAL INFILE '../data/employees.csv'
+LOAD DATA LOCAL INFILE '<insert absolute path here>'
 INTO TABLE employees
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
-(employeeID, employeeName, title, city, country, reportsTo);
+(employeeID, employeeName, title, city, country, @reportsTo)
+SET reportsTo = NULLIF(@reportsTo, ''); -- Handle edge case for blank reportsTo (is the owner of the company)
 
 -- Load data into order_details
-LOAD DATA LOCAL INFILE '../data/order_details.csv'
+LOAD DATA LOCAL INFILE '<insert absolute path here>'
 INTO TABLE order_details
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
 (orderID, productID, unitPrice, quantity, discount);
 
 -- Load data into orders
--- Handle this edge case for Date columns
-LOAD DATA LOCAL INFILE '../data/orders.csv'
+LOAD DATA LOCAL INFILE '<insert absolute path here>'
 INTO TABLE orders
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
-(orderID, customerID, employeeID, @orderDate, @requiredDate, @shippedDate, shipperID, freight)
-SET orderDate = STR_TO_DATE(@orderDate, '%m/%d/%Y'),
-    requiredDate = STR_TO_DATE(@requiredDate, '%m/%d/%Y'),
-    shippedDate = STR_TO_DATE(@shippedDate, '%m/%d/%Y');
+(orderID, customerID, employeeID, orderDate, requiredDate, @shippedDate, shipperID, freight)
+SET shippedDate = NULLIF(@shippedDate, ''); -- Handle edge case for blank shippedDate (order not yet shipped)
 
 -- Load data into products
-LOAD DATA LOCAL INFILE '../data/products.csv'
+LOAD DATA LOCAL INFILE '<insert absolute path here>'
 INTO TABLE products
+CHARACTER SET latin1 -- Customer names contains special characters that MySQL cannot import, this normalizes it
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
-(productID, productName, quantityPerUnit, unitPrice, dicsountinued, categoryID);
+(productID, productName, quantityPerUnit, unitPrice, discontinued, categoryID);
 
 -- Load data into shippers
-LOAD DATA LOCAL INFILE '../data/shippers.csv'
+LOAD DATA LOCAL INFILE '<insert absolute path here>'
 INTO TABLE shippers
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
 (shipperID, companyName);
+
+SET FOREIGN_KEY_CHECKS = 1; -- Set it back to true for future queries
